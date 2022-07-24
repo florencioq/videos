6bc9ac5b331d6ceaf5f758534580414fc4035b3b
florencioq@gmail.com <florencioq@gmail.com>
2022-07-24 15:53:33 -0300

    Avanço

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
-    render() {
-        return <div>SearchBar</div>
-    }
+  state = { term: "eoriuwyoiruyweoiurwe" };
 
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
