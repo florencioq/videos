eadaf48c54278718c95e56a535cea829274a4989
Florencio <florencio@ideos.com.br>
2022-08-04 08:57:04 -0300

    avanço

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import './api_key.js'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+    }
+});
+
